9f40ddb7ea8735186d5f270004cd2eecce975b33
Steve L. Nyemba <steve@the-phi.com>
2014-08-28 15:43:00 -0500

    dom.js for handling dom objects

---
+++ dom.js
@@ -0,0 +1,32 @@
+/**
+ * The Phi Technology LLC,
+ * Javascript-x framework version 0.1
+ * 
+ * Handling of DOM objects will rely on DOM object identifiers (ID) with a few exceptions
+ * This file handles DOM objects with a namespace implementation. 
+ * The basic design is a wrapper around WC3 standard interface for a dom
+ */
+
+if(!jx){
+    var jx = {}
+}
+jx.dom = {} ;
+jx.dom.get = {} ;
+/**
+ * This function will return the value of a dom object regardless of the object
+ * @param {type} id
+ * @returns {undefined}
+ */
+jx.dom.get.value = function(id){}
+jx.dom.get.children = function(id){}
+
+jx.dom.set = {} ;
+/**
+ * This function will set a value to a dom object
+ * @param {type} id
+ * @param {type} value
+ * @returns {undefined}
+ */
+jx.dom.set.value= function(id,value){
+    
+}
