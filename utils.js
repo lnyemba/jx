597535a77fa185123d366efc59f3f7f0514807ae
Steve L. Nyemba <steve@3ridu.dhcp.mc.vanderbilt.edu>
2014-08-28 16:04:51 -0500

    utility namespace added

---
+++ utils.js
@@ -0,0 +1,14 @@
+/**
+ * The Phi Technology LLC, Steve L. Nyemba <steve@the-phi.com>
+ * Javascript-x
+ * 
+ * This is a utiliy class that is used across several namespaces because it implements handy utility functionalities and design patterns
+ */
+if(!jx){
+    var jx = {}
+}
+jx.utils = {} ;
+jx.utils.patterns = {}
+jx.utils.patterns.visitor = function(list,pointer){}
+jx.utils.patterns.iterator = function(list,ipointer){}
+
